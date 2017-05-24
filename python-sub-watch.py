1f0b3fca4908b454235c3c100c9970fe66d6cb87
Travis Zillmer <tpzillmer@gmail.com>
2017-05-24 16:20:50 -0500

    Added features to look for additional keywords. Fixed things.

--- python-sub-watch.py
+++ python-sub-watch.py
@@ -1,23 +1,34 @@
 import praw
 
-reddit = praw.Reddit(client_id='ID',
-                     client_secret='SECRET',
-                     user_agent='AGENT')
+reddit = praw.Reddit(client_id='GWQCwKgHbzFZYw',
+                     client_secret=None,
+                     user_agent='test')
 
-currentTitle = ''
-
-def watchSubforKey(sub, key):
-    for submission in reddit.subreddit(sub).new(limit=1):
-        title = submission.title
-        global currentTitle
-        if title != currentTitle:
-            currentTitle = title
-            if key in title:
-                print("A mention of {} has been posted in {}".format(key, sub))
+def watchSubforKey(sub, keyList):
+    currentTitle = ''
+    try:
+        while(True):
+            for submission in reddit.subreddit(sub).new(limit=1):
+                title = submission.title
+                if title != currentTitle:
+                    currentTitle = title
+                    for keyword in keyList:
+                        if keyword in title:
+                            print("A mention of {} has been posted in {}.".format(keyword, sub))
+                            
+    except KeyboardInterrupt:
+        print("Done watching")
 
 if __name__ == "__main__":
+    keyList = []
+    
     sub = input("Please enter a subreddit you would like to watch: ")
     key = input("Please enter a keyword you would like to watch for in {}: ".format(sub))
+    keyList.append(key)
+    
+    while(key.lower() != 'no'):
+        key = input('Would you like to watch for an additional keyword? If not, enter "no": ')
+        if(key.lower()!='no'):
+            keyList.append(key)
 
-    while(1):
-        watchSubforKey(sub, key)
+    watchSubforKey(sub, keyList)
